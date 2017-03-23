b4c9de3dbb7b0ad4e2140eaba494129f9649cd5d
siqinglee <leesi02@ca.com>
2017-03-23 15:34:39 -0400

    add bootstrap

--- wordpress/wp-content/themes/leelamsin/index.php
+++ wordpress/wp-content/themes/leelamsin/index.php
@@ -5,11 +5,15 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
 <?php get_header(); ?>
     
+
 </body>
 
 </html>
